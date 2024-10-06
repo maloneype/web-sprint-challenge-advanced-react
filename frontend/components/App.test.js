f888b61c9f7dd92349871baae7e6b6e571095a81
Paul Maloney <paul@paulmaloney.com>
2024-10-06 16:22:41 -0700

    testing works

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,6 +1,6 @@
-import server from './backend/mock-server'
+import server from '../../backend/mock-server';
 import React from 'react'
-import AppFunctional from './frontend/components/AppFunctional'
+import AppFunctional from '../../frontend/components/AppFunctional'
 import { render, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
