554f64a7423dbb3bc38c4ba9ea0cee399f137fde
Paul Maloney <paul@paulmaloney.com>
2024-10-06 16:11:45 -0700

    grace

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -146,7 +146,7 @@ export default function AppFunctional(props) {
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{coordinateMessage}</h3>
-        <h3 id="steps">You moved {stepCount} times</h3>
+        <h3 id="steps">You moved {stepCount} time{stepCount>1?"s":""}</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
